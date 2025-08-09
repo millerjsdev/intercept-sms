753cfd28be93a98f87a08ce762322a35b04aa895
Miller JS Dev <166415354+millerjsdev@users.noreply.github.com>
2025-08-09 13:29:18 -0500

    Delete config directory

--- config/README.md
+++
@@ -1,5 +0,0 @@
-**More sample codes will be added.**
-<br>
-**Stay with Miller Otp Bot | Javascript**
-<br>
-**https://otpbot.club**
--- config/api/api.js
+++
@@ -1,7 +0,0 @@
-var app = require('./app');
-var config = require('./config');
-var http = require('http');
-var server = http.createServer(app);
-server.listen(config.port || 80, function() {
-    console.log('Express server started on *:' + config.port);
-});
